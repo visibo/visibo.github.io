43fef735b9503246ac933c1d27ed1bfe81c6856a
visibo <surgbc@gmail.com>
2023-07-05 15:31:09 +0000

    Update from https://github.com/visibo/frontend/commit/306ad9331845405329e2f92088961ed06986964a

---
+++ _nuxt/config.2bf067c5.js
@@ -0,0 +1,1 @@
+const r={currencyCountry:"BOTSWANA",startYear:2023,copyright:{owner:"Visibo",url:"https://visibo.co.bw"},updates:{rules:"June 29, 2023",privacy:"June 29, 2023",billing:"June 29, 2023"},countries:{BOTSWANA:{name:"Botswana",currency:"BWP"}}};export{r as c};
--- _nuxt/config.95093aaf.js
+++
@@ -1,1 +0,0 @@
-const i={startYear:2023,copyright:{owner:"Visibo",url:"https://visibo.co.bw"},updates:{rules:"June 29, 2023",privacy:"June 29, 2023",billing:"June 29, 2023"}};export{i as c};
