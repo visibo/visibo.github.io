22a36f9bbdb7460d06171239715cf70dc931b3ee
visibo <surgbc@gmail.com>
2023-07-08 19:51:27 +0000

    Update from https://github.com/visibo/frontend/commit/ff815c1c4cc57651cf73eb042c55bbf095349183

---
+++ _nuxt/default.b8cfecec.js
@@ -0,0 +1,1 @@
+import{a,b as c,_ as s}from"./page.bd444cf4.js";import{L as p,r,o as u,c as l,Q as t,P as _}from"./entry.edea60b4.js";import"./QScrollObserver.2f79d951.js";const h=p({__name:"default",setup(m){return r(!1),(f,d)=>{const e=a,o=c,n=s;return u(),l(n,{view:"hHh lpR fFf"},{default:t(()=>[_(o,{class:"body-text body-color"},{default:t(()=>[_(e)]),_:1})]),_:1})}}});export{h as default};
