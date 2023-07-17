9d5242b07e53e944582a35af54e17b77150df3c0
visibo <surgbc@gmail.com>
2023-07-17 15:05:57 +0000

    Update from https://github.com/visibo/frontend/commit/65aa465ffcbe40ea4d918188493d39b22fb2ba80

---
+++ _nuxt/QToolbar.7075b027.js
@@ -0,0 +1,1 @@
+import{a,f as s,j as r,i as n}from"./entry.89506dec.js";const p=a({name:"QToolbar",props:{inset:Boolean},setup(o,{slots:t}){const e=s(()=>"q-toolbar row no-wrap items-center"+(o.inset===!0?" q-toolbar--inset":""));return()=>r("div",{class:e.value,role:"toolbar"},n(t.default))}});export{p as _};
